bef835d891aa60abfdf879acd1be903e2e9c54c4
Arthur-Barreto <arthurbarreto2905@gmail.com>
2024-06-24 11:38:17 -0500

    boundary check

--- src/bls.cu
+++ src/bls.cu
@@ -119,6 +119,8 @@ int main(int argc, char *argv[]) {
     int blockSize = 128;
     int numBlocks = (n + blockSize - 1) / blockSize;
 
+    auto start = chrono::high_resolution_clock::now();
+
     weight_sum_kernel<<<numBlocks, blockSize>>>(thrust::raw_pointer_cast(d_flux_err.data()), d_weight_sum, n);
     cudaMemcpy(&h_weight_sum, d_weight_sum, sizeof(double), cudaMemcpyDeviceToHost);
 
@@ -131,8 +133,6 @@ int main(int argc, char *argv[]) {
 
     cudaMalloc((void **)&d_result, sizeof(BlsResult));
     cudaMemcpy(d_result, &h_result, sizeof(BlsResult), cudaMemcpyHostToDevice);
-
-    auto start = chrono::high_resolution_clock::now();
 
     bls_kernel<<<numBlocks, blockSize>>>(thrust::raw_pointer_cast(d_time.data()), thrust::raw_pointer_cast(d_flux.data()), thrust::raw_pointer_cast(d_weight.data()), n, d_result);
     cudaMemcpy(&h_result, d_result, sizeof(BlsResult), cudaMemcpyDeviceToHost);
