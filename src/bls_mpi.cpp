884485588e8e848e7292c6ee58839800e9fcc577
Arthur-Barreto <arthurbarreto2905@gmail.com>
2024-07-30 18:12:11 -0500

    log improvedd on mpi

--- src/bls_mpi.cpp
+++ src/bls_mpi.cpp
@@ -40,25 +40,37 @@ int main(int argc, char *argv[]) {
 
     cout << fixed << setprecision(10);
 
-    double total_duration = 0.0;
+    auto start = chrono::high_resolution_clock::now();
     BLSResult result;
+    vector<double> exec_time = vector<double>(samples);
 
     for (int i = 0; i < samples; ++i) {
         auto iteration_start = chrono::high_resolution_clock::now();
         vector<SPECParameters> s_params = spec_generator_gambiarra(time);
         result = bls_mpi(time, flux, flux_err, s_params, rank, size);
         auto iteration_end = chrono::high_resolution_clock::now();
-        total_duration += chrono::duration_cast<chrono::milliseconds>(iteration_end - iteration_start).count();
+        exec_time[i] = chrono::duration_cast<chrono::milliseconds>(iteration_end - iteration_start).count();
     }
 
-    auto average_duration = total_duration / samples;
+    auto average_duration = accumulate(exec_time.begin(), exec_time.end(), 0.0) / samples;
+    auto min_duration = *min_element(exec_time.begin(), exec_time.end());
+    auto max_duration = *max_element(exec_time.begin(), exec_time.end());
+    auto std_dev = sqrt(accumulate(exec_time.begin(), exec_time.end(), 0.0,
+                                   [average_duration](double accumulator, double value) {
+                                       return accumulator + pow(value - average_duration, 2);
+                                   }) /
+                        samples);
 
     if (rank == 0) {
+
         cout << "Best period: " << result.best_period << endl;
         cout << "Best duration: " << result.best_duration << endl;
         cout << "Best phase: " << result.best_phase << endl;
         cout << "Best d_value: " << result.best_d_value << endl;
         cout << "Average execution time: " << average_duration << " ms" << endl;
+        cout << "Min execution time: " << min_duration << " ms" << endl;
+        cout << "Max execution time: " << max_duration << " ms" << endl;
+        cout << "Standard deviation: " << std_dev << " ms" << endl;
     }
 
     MPI_Finalize();
